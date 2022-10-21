759fe1dedcbd4de794cb29296f1125747ff7732a
trevor dewindt <trevor.dewindt@gmail.com>
2022-10-21 16:02:50 -0600

    add final code

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toyList, onDeleteToy, onLikeToy }) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toyList.map(toy => <ToyCard key={toy.id} toy={toy} onDeleteToy={onDeleteToy} onLikeToy={onLikeToy} />)}</div>
   );
 }
 
